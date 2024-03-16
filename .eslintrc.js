086979e0fef5462bcb1bea6834bc9fd179c68cbe
Tomas Rambousek <t.rambousek@gmail.com>
2024-03-16 12:18:06 +0100

    Project setup

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  extends: [
+    "universe/native",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [
+    "@typescript-eslint",
+    "react-native",
+    "react-hooks",
+    "prettier",
+  ],
+  rules: {
+    quotes: ["error", "single", { avoidEscape: true }],
+    semi: ["error", "never"],
+    "no-empty-function": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "linebreak-style": ["error", "unix"],
+    "prettier/prettier": "error",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "prettier/prettier": "error",
+    "react-native/no-unused-styles": "error",
+    "react-native/split-platform-components": "error",
+    "react-native/no-inline-styles": "error",
+    "react-native/no-color-literals": "error",
+    "react-native/no-raw-text": "error",
+    "react-native/no-single-element-style-arrays": "error",
+  },
+};
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Task:
+Develop a simple mobile app which uses the GitHub REST API to display the list of public
+repositories belonging to an organization, plus a very simple settings screen.
+
+## UX Design
+1. An item in the repositories list shows the repository name and description.
+2. Settings is a static screen with hardcoded text and image. Any image can be used.
+3. Any color and font can be used to develop this application.
+
+
+# Estimations:
+1. Project setup                    1h
+2. Navigation with empty pages      1h
+3. Implement settings screen        1h
+4. Implement Repositories screen    1h
